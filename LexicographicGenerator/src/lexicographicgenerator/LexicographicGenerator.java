958703b415743399a0a45358352af3fbc1e53c25
Edwin Richbald A. Salinas <easalinas@fit.edu.ph>
2016-09-10 20:59:37 +0800

    Prints string as an array

--- LexicographicGenerator/src/lexicographicgenerator/LexicographicGenerator.java
+++ LexicographicGenerator/src/lexicographicgenerator/LexicographicGenerator.java
@@ -1,7 +1,10 @@
 package lexicographicgenerator;
 public class LexicographicGenerator {
     public static void main(String[] args) {
-        
+        String charset = "ABC";
+        for(int i = 0; i<charset.length(); i++){
+            System.out.println(charset.charAt(i));
+        }
     }
     
 }
